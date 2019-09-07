5a4981990729807841ffefe6f98d9bc12fdb876e
Thomas Seary <tseary@gmail.com>
2019-09-07 01:37:32 -0230

    Char array implementation
    Replaced multiple layer arrays with single char array.

--- SpaceInvaders/Display.ino
+++ SpaceInvaders/Display.ino
@@ -2,15 +2,9 @@
 void printDisplay() {
 	for (uint8_t y = SCREEN_HEIGHT - 1; y < SCREEN_HEIGHT; y--) {
 		for (uint8_t x = SCREEN_WIDTH - 1; x < SCREEN_WIDTH; x--) {
-			if (isMonster(x, y)) {
-				Serial.print("MM");
-			} else if (isPlayer(x, y)) {
-				Serial.print("^^");
-			} else if (isSpaceship(x, y)) {
-				Serial.print("/\\");
-			} else {
-				Serial.print(".'");
-			}
+			char c = field[x][y];
+			Serial.print(' ');
+			Serial.print(c ? c : ' ');
 		}
 		Serial.println();
 	}
--- SpaceInvaders/SpaceInvaders.ino
+++ SpaceInvaders/SpaceInvaders.ino
@@ -8,12 +8,54 @@ const uint8_t
 SCREEN_WIDTH = 16,
 SCREEN_HEIGHT = 16,
 PLAYER_HEIGHT = 2,
-SHIP_HEIGHT = 1,
-SHIP_OFFSET = SCREEN_HEIGHT - SHIP_HEIGHT;
+SPACESHIP_HEIGHT = 1,
+SHIP_OFFSET = SCREEN_HEIGHT - SPACESHIP_HEIGHT;
 
-uint32_t monsterLayer[SCREEN_HEIGHT];
-uint32_t playerLayer[PLAYER_HEIGHT];
-uint32_t shipLayer[SHIP_HEIGHT];
+const uint8_t
+ALIEN_ROWS = 3,
+ALIEN_COLS = 4,
+ALIEN_W = 2,
+ALIEN_H = 2,
+ALIEN_PITCH_X = 3,	// Horizontal distance from one alien to the next on the field
+ALIEN_PITCH_Y = 3,	// Vertical distance from one alien to the next on the field
+TOTAL_ALIENS = ALIEN_ROWS * ALIEN_COLS;
+
+// The position of the lower-right alien on the field.
+uint8_t xGridOrigin, yGridOrigin;
+
+char field[SCREEN_WIDTH][SCREEN_HEIGHT];
+
+// Field symbols
+const char
+F_PLAYER = '@',
+F_PLAYER_BULLET = '^',
+F_ALIEN_BULLET = 'v',
+F_SPACSHIP = '$',
+F_SHIELD = '#',
+F_ALIEN_0 = 'A';
+
+// The field is the size of the screen and has one char per pixel.
+// The char identifies what is on-screen.
+// 
+// field coordinates
+//    151413121110 9 8 7 6 5 4 3 2 1 0
+// 15  . . . . $ $ $ . . . . . . . . .
+// 14  . . . . . . . . . . . . . . . .
+// 13  . . . . . . . . . . . . . . . .
+// 12  . . L L . K K . J J . I I . . .
+// 11  . . L L . K K . J J . I I . . .
+// 10  . . . . . . . . . . . . . . . .
+//  9  . . H H . G G . F F . E E . . .
+//  8  . . . H . G . . . F . E . . . .
+//  7  . . . . . . . . . . . . . . . .
+//  6  . . . ^ . C C . B B . A A . . .
+//  5  . . . . . . C . B . . . A . . .
+//  4  . . v . . . . . . . . . . . . .
+//  3  . . . . . . . . . . . v . . . .
+//  2  . . # # . . . # # . . . # # . .
+//  1  . . . . . @ . . . . . . . . . .
+//  0  . . . . @ @ @ . . . . . . . . .
+// 
 
 bool moveLeft = false;
 bool moveDown = false;
@@ -24,15 +66,15 @@ void setup() {
 	delay(500);
 	Serial.println("Space Invaders");
 
-	shipLayer[0] = 0b0000000000111000;
+	newGame();
 
-	monsterLayer[SCREEN_HEIGHT - 2] = 0b1101101101100000;
-	monsterLayer[SCREEN_HEIGHT - 3] = 0b0101000101000000;
-	monsterLayer[SCREEN_HEIGHT - 5] = 0b1000101000100000;
-	monsterLayer[SCREEN_HEIGHT - 6] = 0b1101101101100000;
-
-	playerLayer[1] = 0b0000100000000000;
-	playerLayer[0] = 0b0001110000000000;
+	// DEBUG
+	uint8_t xPlayer = 11,
+		yPlayer = 0;
+	field[xPlayer][yPlayer] = F_PLAYER;
+	field[xPlayer + 1][yPlayer] = F_PLAYER;
+	field[xPlayer - 1][yPlayer] = F_PLAYER;
+	field[xPlayer][yPlayer + 1] = F_PLAYER;
 
 	printDisplay();
 }
@@ -40,7 +82,7 @@ void setup() {
 void loop() {
 	// Move monsters
 	if (!moveDown) {
-		moveDown = !(moveLeft ? tryShiftMonstersLeft() : tryShiftMonstersRight());
+		moveDown = !(moveLeft ? tryShiftAliensLeft() : tryShiftAliensRight());
 	}
 	if (moveDown) {
 		shiftMonstersDown();
@@ -52,45 +94,83 @@ void loop() {
 	delay(1000);
 }
 
-bool tryShiftMonstersLeft() {
+void newGame() {
+	// Reset the aliens
+	// Start with the grid of aliens touching the left side of the field...
+	xGridOrigin = SCREEN_WIDTH - ((ALIEN_COLS - 1) * ALIEN_PITCH_X + ALIEN_W);
+	// ..and as close as possible to the top of the screen, leaving space for spaceships.
+	yGridOrigin = SCREEN_HEIGHT - ((ALIEN_ROWS - 1) * ALIEN_PITCH_Y + ALIEN_H) - SPACESHIP_HEIGHT;
+
+	// Clear the field
+	memset(field, '\0', SCREEN_WIDTH * SCREEN_HEIGHT);
+
+	// Draw the aliens
+	for (uint8_t i = 0; i < ALIEN_COLS; i++) {
+		for (uint8_t j = 0; j < ALIEN_ROWS; j++) {
+			uint8_t x = xGridOrigin + i * ALIEN_PITCH_X,
+				y = yGridOrigin + j * ALIEN_PITCH_Y;
+			char c = F_ALIEN_0 + i + j * ALIEN_COLS;
+			field[x][y] = c;
+			field[x][y + 1] = c;
+			field[x + 1][y + 1] = c;
+		}
+	}
+}
+
+bool tryShiftAliensLeft() {
 	for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
-		if (isMonster(SCREEN_WIDTH - 1, y)) {
+		if (isAlien(SCREEN_WIDTH - 1, y)) {
 			return false;
 		}
 	}
 	for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
-		monsterLayer[y] <<= 1;
+		for (uint8_t x = 1; x < SCREEN_WIDTH; x++) {
+			if (isAlien(x, y)) {
+				field[x - 1][y] = field[x][y];
+				field[x][y] = '\0';
+			}
+		}
 	}
 	return true;
 }
 
-bool tryShiftMonstersRight() {
+bool tryShiftAliensRight() {
 	for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
-		if (isMonster(0, y)) {
+		if (isAlien(0, y)) {
 			return false;
 		}
 	}
 	for (uint8_t y = 0; y < SCREEN_HEIGHT; y++) {
-		monsterLayer[y] >>= 1;
+		for (uint8_t x = SCREEN_WIDTH - 2; x < SCREEN_WIDTH; x--) {
+			if (isAlien(x, y)) {
+				field[x + 1][y] = field[x][y];
+				field[x][y] = '\0';
+			}
+		}
 	}
 	return true;
 }
 
 void shiftMonstersDown() {
 	for (uint8_t y = 1; y < SCREEN_HEIGHT; y++) {
-		monsterLayer[y - 1] = monsterLayer[y];
+		for (uint8_t x = 0; x < SCREEN_WIDTH; x++) {
+			if (isAlien(x, y)) {
+				field[x][y - 1] = field[x][y];
+				field[x][y] = '\0';
+			}
+		}
 	}
 }
 
 bool isSpaceship(uint8_t x, uint8_t y) {
-	y -= SHIP_OFFSET;
-	return y < SHIP_HEIGHT ? (shipLayer[y] & ((uint32_t)1 << x)) : false;
+	return field[x][y] == F_SPACSHIP;
 }
 
-bool isMonster(uint8_t x, uint8_t y) {
-	return y < SCREEN_HEIGHT ? (monsterLayer[y] & ((uint32_t)1 << x)) : false;
+bool isAlien(uint8_t x, uint8_t y) {
+	if (x < SCREEN_WIDTH && y < SCREEN_HEIGHT) return false;
+	return (field[x][y] - F_ALIEN_0) < TOTAL_ALIENS;
 }
 
 bool isPlayer(uint8_t x, uint8_t y) {
-	return y < PLAYER_HEIGHT ? (playerLayer[y] & ((uint32_t)1 << x)) : false;
+	return field[x][y] == F_PLAYER;
 }
